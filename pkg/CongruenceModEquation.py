d7e5da10762a05b6ff790d9bb3cdb66be6f76d30
AlwaysStudent <49239915+AlwaysStudent@users.noreply.github.com>
2020-07-03 19:56:09 +0800

    fuck the changing from latex str to html

--- pkg/CongruenceModEquation.py
+++ pkg/CongruenceModEquation.py
@@ -2,25 +2,66 @@
 Author: AlwaysStudent
 PythonVersion: 3.7
 """
+import math
 import base
+import ExtendedEuclidean
 
 
 class CongruenceModEquation(base.Value, base.Process):
-    def __init__(self, a, b, p, num=1):
-        self.a = a
-        self.b = b
-        self.p = p
-        self.num = num
+    def __init__(self, a, b, m, num=1):
+        try:
+            assert a % num == 0 and b % num == 0 and m % num == 0 and num == math.gcd(math.gcd(a, b), m)
+        except AssertionError:
+            num = -1
+        finally:
+            self.a = a
+            self.b = b
+            self.m = m
+            self.num = num
 
     def process(self) -> str:
-        pass
+        result = "$$ \\begin{aligned} "
+        if self.num != 1:
+            result += "&\\because (" + ", ".join([str(self.a), str(self.b), str(self.m)]) + ") = " + str(self.num) + "\\\\ &\\therefore"
+        result += "{" + str(self.a) + "} {x} \;\equiv\; {" + str(self.b) + "}\;mod\;{" + str(self.m) + "} \\\\"
+        a = self.a // self.num
+        b = self.b // self.num
+        m = self.m // self.num
+        result += "&\\therefore{" + str(a) + "} {x} \;\equiv\; {" + str(b) + "}\;mod\;{" + str(m) + "} \\\\ \\end{aligned} $$"
+        temp = ExtendedEuclidean.ExtendedEuclidean(a, m)
+        result += temp.process()
+        t, x, _ = temp.value()
+        if t != 1:
+            result += "$$ \\begin{aligned} \\because (" + str(a) + ", " + str(m) + ") \\not= 1 \\\\ \\therefore No Solution \\end{aligned} $$"
+            return result
+        else:
+            b = x * b % m
+            result += "$$ \\begin{aligned} \\therefore {x} \;&\equiv\; {" + str(b) + "}\;mod\;{" + str(m) + "} \\\\ \\end{aligned} $$ $$\\begin{aligned}\\therefore  "
+        for i in range(self.num):
+            result += "{x} \;&\equiv\; {" + str(b + i * m) + "}\;mod\;{" + str(self.m) + "} \\\\"
+        return result + "\\end{aligned} $$"
 
     def value(self):
-        pass
+        result = []
+        if self.num == -1:
+            return result
+        a = self.a // self.num
+        b = self.b // self.num
+        m = self.m // self.num
+        temp = ExtendedEuclidean.ExtendedEuclidean(a, m)
+        t, x, _ = temp.value()
+        if t != 1:
+            return result
+        else:
+            b = x * b % m
+        for i in range(self.num):
+            result.append(b + i * m)
+        return result
 
 
 def main():
-    pass
+    x = CongruenceModEquation(28, 21, 35, 7)
+    print(x.process())
 
 
 if __name__ == '__main__':
--- pkg/latex.py
+++ pkg/latex.py
@@ -2,42 +2,20 @@
 Author: AlwaysStudent
 PythonVersion: 3.7
 """
-from pylatex import Document, Section
-import matplotlib
-from matplotlib import rcParams
-import matplotlib.pyplot as plt
-rcParams['text.latex.preamble'] = r'\usepackage{amsmath}\usepackage{amssymb}'
-matplotlib.rcParams['text.usetex'] = True
-matplotlib.rcParams['text.latex.unicode'] = True
+import markdown2 as md
+import codecs
 
-def renderlatex(formula, fontsize=18, dpi=300, format_='png'):
-    """
-    Renders LaTeX formula into image.
-    """
-    fig = plt.figure(figsize=(0.01, 0.01))
-    fig.text(0, 0, u'${}.format(formula)', fontsize=fontsize)
-    buffer_ = io.BytesIO()
-    fig.savefig(buffer_, dpi=dpi, transparent=True, format=format_, bbox_inches='tight', pad_inches=0.0)
-    return buffer_.getvalue()
-
-def formula2img(str_latex, out_file, img_size=(5, 3), font_size=16):
-    fig = plt.figure(figsize=img_size)
-    ax = fig.add_axes([0, 0, 1, 1])
-    ax.get_xaxis().set_visible(False)
-    ax.get_yaxis().set_visible(False)
-    ax.set_xticks([])
-    ax.set_yticks([])
-    plt.text(0.5, 0.5, str_latex, fontsize=font_size, verticalalignment='center', horizontalalignment='center')
-    plt.savefig(out_file)
 
 def main():
-    # doc = Document(default_filepath="basic", documentclass="article")
-    # with doc.create(Section("math")):
-    #     doc.append(r"$$ \begin{aligned} {8656} &= {1} * {7780} + {876} \\{7780} &= {8} * {876} + {772} \\{876} &= {1} * {772} + {104} \\{772} &= {7} * {104} + {44} \\{104} &= {2} * {44} + {16} \\{44} &= {2} * {16} + {12} \\{16} &= {1} * {12} + {4} \\{12} &= {3} * {4} + {0} \\ \end{aligned} $$ $$ \begin{aligned} {4} &= ({1}) * {16} + ({-1}) * {12} \\ &= ({1}) * {16} + ({-1}) * ({44} - ({2}) * {16}) \\ &= ({-1}) * {44} + ({3}) * {16} \\ &= ({-1}) * {44} + ({3}) * ({104} - ({2}) * {44}) \\ &= ({3}) * {104} + ({-7}) * {44} \\ &= ({3}) * {104} + ({-7}) * ({772} - ({7}) * {104}) \\ &= ({-7}) * {772} + ({52}) * {104} \\ &= ({-7}) * {772} + ({52}) * ({876} - ({1}) * {772}) \\ &= ({52}) * {876} + ({-59}) * {772} \\ &= ({52}) * {876} + ({-59}) * ({7780} - ({8}) * {876}) \\ &= ({-59}) * {7780} + ({524}) * {876} \\ &= ({-59}) * {7780} + ({524}) * ({8656} - ({1}) * {7780}) \\ &= ({524}) * {8656} + ({-583}) * {7780} \\ \end{aligned} $$")
-    #
-    # doc.generate_pdf(filepath=".")
-    # x = r"\usepackage{amsmath}\usepackage{amssymb}\begin{aligned} $ {8656} &= {1} * {7780} + {876} \ {7780} &= {8} * {876} + {772} \ {876} &= {1} * {772} + {104} \{772} &= {7} * {104} + {44} \{104} &= {2} * {44} + {16} \{44} &= {2} * {16} + {12} \{16} &= {1} * {12} + {4} \{12} &= {3} * {4} + {0} \  $\end{aligned} \begin{aligned}$  {4} &= ({1}) * {16} + ({-1}) * {12} \ &= ({1}) * {16} + ({-1}) * ({44} - ({2}) * {16}) \ &= ({-1}) * {44} + ({3}) * {16} \ &= ({-1}) * {44} + ({3}) * ({104} - ({2}) * {44}) \ &= ({3}) * {104} + ({-7}) * {44} \ &= ({3}) * {104} + ({-7}) * ({772} - ({7}) * {104}) \ &= ({-7}) * {772} + ({52}) * {104} \ &= ({-7}) * {772} + ({52}) * ({876} - ({1}) * {772}) \ &= ({52}) * {876} + ({-59}) * {772} \ &= ({52}) * {876} + ({-59}) * ({7780} - ({8}) * {876}) \ &= ({-59}) * {7780} + ({524}) * {876} \ &= ({-59}) * {7780} + ({524}) * ({8656} - ({1}) * {7780}) \ &= ({524}) * {8656} + ({-583}) * {7780} \ $ \end{aligned}"
-    # formula2img(x, r".1.png", img_size=(20, 10), font_size=10)
+    with codecs.open("1.md", mode='r', encoding='utf-8') as mdfile:
+        md_text = mdfile.read()
+    with codecs.open("./katex/Katex.min.css", mode='r', encoding='utf-8') as cssfile:
+        css_text = cssfile.read()
+    extras = ['code-friendly', 'fenced-code-blocks', 'footnotes']
+    html_text = md.markdown(md_text, extras=extras)
+    html_name = "1.html"
+    with codecs.open(html_name, mode='w', encoding='utf-8') as output_file:
+        output_file.write(css_text + html_text)
 
 
 if __name__ == '__main__':
